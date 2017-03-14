1ed853362b789fd9e82de991df266adb0b6541f2
n0rtan <n0rtan@inbox.ru>
2017-03-15 00:48:55 +0300

    changes configurations adds static resources

--- gradle-spring-mvc--xml--jsp/src/main/java/ru/n0rtan/referenceapp/controller/DefaultController.java
+++ gradle-spring-mvc--xml--jsp/src/main/java/ru/n0rtan/referenceapp/controller/DefaultController.java
@@ -1,4 +1,4 @@
-package ru.n0rtan.reference.controller;
+package ru.n0rtan.referenceapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,10 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class ReferenceController {
+public class DefaultController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
+
 
         model.addAttribute("message", "Assigned Message");
 
--- gradle-spring-mvc--xml--jsp/src/main/webapp/WEB-INF/pages/index.jsp
+++ gradle-spring-mvc--xml--jsp/src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,7 +1,14 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<spring:url value="/resources/js/main.js" var="mainjs" />
 <html>
 <head>
+
     <title>Gradle-Spring-MVC / XML / JSP - Reference Application</title>
+
+    <link href="<c:url value="/resources/css/main.css" />" rel="stylesheet" />
+    <script src="${mainjs}"></script>
+
 </head>
 <body>
 
